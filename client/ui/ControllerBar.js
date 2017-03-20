62065406ab734ae28e380edc5cbad51b6772c56a
Alex Hamlin <alex@alexhamlin.co>
2017-03-19 18:11:16 -0700

    Add nbsp's to controller bar
    So I can feel like I'm back in the 90's, I guess?

--- client/ui/ControllerBar.js
+++ client/ui/ControllerBar.js
@@ -26,26 +26,26 @@ export default class ControllerBar extends React.Component {
     return (
       <form className="form-inline text-center lead" id="tuner" onSubmit={this.handleSubmit}>
 	<div className="form-group">
-	  <label htmlFor="channel">Watch </label>
+	  <label htmlFor="channel">Watch&nbsp;</label>
 	  <ChannelSelector
 	    list={this.props.channels}
 	    selected={this.props.tuneData.channel}
 	    onChange={this.props.onChannelChanged} />
 	</div>
-
+	&nbsp;
 	<div className="form-group">
-	  <label htmlFor="profile"> at </label>
+	  <label htmlFor="profile">at&nbsp;</label>
 	  <ProfileSelector
 	    profiles={this.props.profiles}
 	    selected={findKey(this.props.profiles, this.props.tuneData.profile)}
 	    onChange={this.props.onProfileChanged} />
-	  <label htmlFor="profile"> quality</label>
+	  <label htmlFor="profile">&nbsp;quality</label>
 	</div>
-
+	&nbsp;
 	<button type="submit" className="btn btn-default">
 	  <span className="glyphicon glyphicon-play"></span>
 	</button>
-
+	&nbsp;
 	<button type="button" className="btn btn-default" onClick={this.handleStop}>
 	  <span className="glyphicon glyphicon-stop"></span>
 	</button>
