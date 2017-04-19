490197128c395cbc1943a8aae6aefff78cc76caa
Alex Hamlin <alex@alexhamlin.co>
2017-04-18 23:57:17 -0700

    Add and configure ESLint
    Install ESLint as a development dependency and create an initial
    configuration based on answers to some simple questions. For now I'm not
    attempting to use a formal style guide like Airbnb's (Google's doesn't
    seem to account for ES6 at this time), though this could potentially
    change in the future.

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "indent": [ "error", 2 ],
+        "linebreak-style": [ "error", "unix" ],
+        "quotes": [ "error", "single" ],
+        "semi": [ "error", "always" ]
+    }
+};
