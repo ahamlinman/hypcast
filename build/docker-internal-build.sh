efca13b0374a481533ffab72807d006b97dc4172
Alex Hamlin <alex@alexhamlin.co>
2017-11-25 01:00:36 -0800

    Work around Yarn --ignore-optional bug
    (Message redacted 2019-02-10. It was a shameful and unacceptable
    overreaction to a minor bug in an open source, community-maintained tool
    that has proven exceptionally useful to me and to many others.)

--- build/docker-internal-build.sh
+++ build/docker-internal-build.sh
@@ -2,8 +2,9 @@
 
 set -ev
 
-# Install all build-time dependencies using Yarn
-yarn install --ignore-optional
+# Install all build-time dependencies using Yarn. Usually I'd ignore optional
+# packages, but Yarn is buggy here: https://github.com/yarnpkg/yarn/issues/4876
+yarn install
 
 # Run the full build for server and client
 yarn run build:mini
