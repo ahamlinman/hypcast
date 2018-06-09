4a114085e9a604591aa90786a9d9eda4118ef6f4
Alex Hamlin <alex@alexhamlin.co>
2018-06-08 21:37:19 -0700

    Enable strictPropertyInitialization for client code
    This required some HlsVideoPlayer changes. Mostly just adding
    boilerplate since things aren't always initialized in the constructor.

--- client/ui/HlsVideoPlayer.tsx
+++ client/ui/HlsVideoPlayer.tsx
@@ -2,28 +2,36 @@ import * as React from 'react';
 import * as Hls from 'hls.js/dist/hls.light.js';
 
 export default class HlsVideoPlayer extends React.Component<{ src: string }, {}> {
-  private hls: Hls;
-  private video: HTMLVideoElement;
+  private hls: Hls | null = null;
+  private video: HTMLVideoElement | null = null;
 
   componentDidMount() {
+    if (!this.video) {
+      throw new Error('null <video> after HlsVideoPlayer mount');
+    }
+
     this.hls = new Hls();
     this.hls.loadSource(this.props.src);
     this.hls.attachMedia(this.video);
-    this.hls.on(Hls.Events.MANIFEST_PARSED, () => this.video.play());
+    this.hls.on(Hls.Events.MANIFEST_PARSED, () => { if (this.video) { this.video.play(); } });
   }
 
   componentWillUnmount() {
-    this.video.pause();
-    this.hls.detachMedia();
-    this.hls.destroy();
+    if (this.video) {
+      this.video.pause();
+      this.video = null;
+    }
+
+    if (this.hls) {
+      this.hls.detachMedia();
+      this.hls.destroy();
+      this.hls = null;
+    }
   }
 
   render() {
     return (
-      <video
-        ref={(video) => { if (video !== null) { this.video = video; } }}
-        controls={true}
-      />
+      <video ref={(video) => { this.video = video; }} controls={true} />
     );
   }
 }
