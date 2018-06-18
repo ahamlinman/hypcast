2c167f5f8cb9f36ae12e20368533c79273ce3f37
Alex Hamlin <alex@alexhamlin.co>
2018-06-17 17:22:27 -0700

    Remove machina from the client
    This change drops over 50 KiB from the minified client by replacing the
    machina state machine with a small, perfectly compatible TypeScript
    class. Webpack automatically provides a simple EventEmitter
    implementation for us to extend.

--- client/controller.ts
+++ client/controller.ts
@@ -1,32 +1,26 @@
-import machina from 'machina';
 import * as socketio from 'socket.io-client';
+import { EventEmitter } from 'events';
 
 import { TuneData } from '../models/TuneData';
 
 /**
- * This FSM actually used to control the entire Hypcast UI. But now that it's
- * been stripped down to this tiny core, its function may not be immediately
- * obvious.
+ * This class uses Socket.io to synchronize the client with a state machine on
+ * the Hypcast server. On each transition of the server's state machine, this
+ * client will update its internal state and emit an event. Downstream, we can
+ * use these events to do something useful (for example, telling React to
+ * re-render the UI).
  *
- * This state machine uses Socket.io to synchronize itself with a nearly
- * equivalent state machine on the Hypcast server. On each transition of the
- * server's state machine, this client state machine will transition as well
- * (and emit an event indicating that it did so). Downstream, we can use these
- * transition events to do something useful (for example, telling React to
- * re-render the UI based on the new state).
+ * This class also provides functions for the client to modify the server's
+ * state. Obviously the server should control whether we are tuning a channel
+ * or waiting for video to buffer, but clients control when the tuner starts
+ * and stops. On these actions, we actually ask the server to modify *its*
+ * state machine, and *its* resulting transitions get reflected on the client.
+ * This ensures that everything stays perfectly syncrhonized across all
+ * clients.
  *
- * This machine also provides functions for the client to modify the state of
- * the server machine. Obviously the server should control whether we are
- * tuning a channel or waiting for video to buffer, but clients control when
- * the tuner starts and stops. On these actions, we actually ask the server to
- * modify *its* state machine, and *its* resulting transitions get reflected on
- * the client. This ensures that everything stays perfectly syncrhonized across
- * all clients.
- *
- * One additional small detail that isn't fully reflected here: When we connect
- * to the server, it will immediately fire off an event over the socket to
- * transition *us* to its state. That's why nothing explicit happens here to
- * sync us on initialization.
+ * One more small detail: When we connect to the server, it will immediately
+ * fire off an event over the socket to transition *us* to its state. That's
+ * why nothing explicit happens here to sync us on initialization.
  */
 
 interface Transition {
@@ -34,45 +28,40 @@ interface Transition {
   tuneData: TuneData;
 }
 
-export default machina.Fsm.extend({
-  initialState: 'connecting',
+export default class HypcastController extends EventEmitter {
+  private _state: string = 'connecting';
+  private _socket: SocketIOClient.Emitter;
 
-  states: {
-    connecting: {
-      // TODO: Better typings for machina
-      _onEnter(this: any) {
-        if (this.socket) {
-          return;
+  constructor() {
+    super();
+
+    this._socket = socketio()
+      .on('transition', ({ toState, tuneData }: Transition) => {
+        if (tuneData) {
+          this.emit('updateTuning', tuneData);
         }
 
-        this.socket = socketio()
-          .on('transition', ({ toState, tuneData }: Transition) => {
-            if (tuneData) {
-              this.emit('updateTuning', tuneData);
-            }
+        this.state = toState;
+      })
+      .on('disconnect', () => {
+        this.state = 'connecting';
+      });
+  }
 
-            this.transition(toState);
-          })
-          .on('disconnect', () => {
-            this.transition('connecting');
-          });
-      },
-    },
+  get state() {
+    return this._state;
+  }
 
-    inactive: {},
-
-    tuning: {},
-
-    buffering: {},
-
-    active: {},
-  },
+  set state(toState: string) {
+    this._state = toState;
+    this.emit('transition');
+  }
 
   tune(tuneData: TuneData) {
-    this.socket.emit('tune', tuneData);
-  },
+    this._socket.emit('tune', tuneData);
+  }
 
   stop() {
-    this.socket.emit('stop');
-  },
-});
+    this._socket.emit('stop');
+  }
+}
