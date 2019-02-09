7191476eb0ed97cd470dd2d55602e0287df90c8b
Alex Hamlin <alex@alexhamlin.co>
2019-02-09 15:26:03 -0800

    Declare machina.Fsm property

--- server/machina.d.ts
+++ server/machina.d.ts
@@ -3,4 +3,8 @@
 //
 // [0]: https://github.com/ifandelse/machina.js/issues/158
 // [1]: https://github.com/DefinitelyTyped/DefinitelyTyped/issues/25075
-declare module 'machina';
+declare module 'machina' {
+  // export as namespace machina;
+
+  export const Fsm: any;
+}
