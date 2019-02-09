d3e1d3594a974038506040107c980ef544a90936
Alex Hamlin <alex@alexhamlin.co>
2019-02-09 15:14:11 -0800

    Separate machina typings into a new file

--- server/machina.d.ts
+++ server/machina.d.ts
@@ -1,7 +1,3 @@
-// TODO: The definitions from DefinitelyTyped aren't entirely accurate at the
-// moment ("kill" and "complexFilter" in particular).
-declare module 'fluent-ffmpeg';
-
 // TODO: Declare stricter types. Consider contributing to machina.js itself [0]
 // or DefinitelyTyped [1] (but preferably the former).
 //
