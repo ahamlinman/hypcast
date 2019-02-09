d570cfd881ae63394939b38e6ef1171073f20463
Alex Hamlin <alex@alexhamlin.co>
2019-02-09 09:36:56 -0800

    Update note about fluent-ffmpeg types
    The thing with console logging was fixed in DefinitelyTyped, and never
    wrong in the documentation in the first place (at least in the README; I
    don't know if I looked at something else or was completely mistaken).
    
    However, there are still a couple other issues preventing my immediate
    adoption of these types.

--- server/types.d.ts
+++ server/types.d.ts
@@ -1,13 +1,5 @@
-// TODO: The documentation and type definitions for fluent-ffmpeg are
-// incorrect. Both claim that the "logger" property given to the constructor
-// must have a "warning" method. However, the relevant function of "console" is
-// called "console.warn", and I've confirmed that this is *actually* the
-// function the library calls.
-
-// Even after fixing this, I think there might be other issues with the typings
-// and/or my usage of the library. The real solution is to contribute back to
-// fluent-ffmpeg and DefinitelyTyped, but note that the full set of fixes may
-// be non-trivial.
+// TODO: The definitions from DefinitelyTyped aren't entirely accurate at the
+// moment ("kill" and "complexFilter" in particular).
 declare module 'fluent-ffmpeg';
 
 // TODO: Declare stricter types. (Ideally, contribute to DefinitelyTyped. See
