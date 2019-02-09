c18c391a61bc2e5b2245133e0e92cd758976a5b8
Alex Hamlin <alex@alexhamlin.co>
2019-02-09 09:45:38 -0800

    Update note about machina.js typings
    There's a push, with some support from the author, to get typings into
    machina.js itself.

--- server/types.d.ts
+++ server/types.d.ts
@@ -2,6 +2,9 @@
 // moment ("kill" and "complexFilter" in particular).
 declare module 'fluent-ffmpeg';
 
-// TODO: Declare stricter types. (Ideally, contribute to DefinitelyTyped. See
-// https://github.com/DefinitelyTyped/DefinitelyTyped/issues/25075.)
+// TODO: Declare stricter types. Consider contributing to machina.js itself [0]
+// or DefinitelyTyped [1] (but preferably the former).
+//
+// [0]: https://github.com/ifandelse/machina.js/issues/158
+// [1]: https://github.com/DefinitelyTyped/DefinitelyTyped/issues/25075
 declare module 'machina';
