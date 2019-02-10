672418d160743df8261180850981b2fd51256f65
Alex Hamlin <alex@alexhamlin.co>
2019-02-10 13:13:44 -0800

    Attempt to type BehavioralFsm
    This honestly isn't as clean as I was hoping for at first. I guess I was
    hoping for lots of extensions and mixins and stuff, but it doesn't fit
    the overall model. Explicitness is probably better anyways.

--- server/machina.d.ts
+++ server/machina.d.ts
@@ -31,21 +31,13 @@ declare module 'machina' {
 
   export interface FsmOptions {
     initialState?: string;
-    states?: States;
     eventListeners?: EventListeners;
+    states?: States;
     namespace?: string;
     initialize?: () => void;
   }
 
-  export class Fsm {
-    new(options: FsmOptions): Fsm;
-    static extend(options: FsmOptions): typeof Fsm;
-
-    initialState: string;
-    eventListeners: EventListeners;
-    states: States;
-    inputQueue: any[];
-    namespace?: string;
+  export interface ClientInstance {
     targetReplayState: any;
     state: string;
     priorState: string;
@@ -53,6 +45,54 @@ declare module 'machina' {
     currentAction: string;
     currentActionArgs: any[];
     initialize: () => void;
+    inputQueue: any[];
+    inExitHandler: boolean;
+  }
+
+  export interface Client {
+    __machina__: ClientInstance;
+  }
+
+  export class BehavioralFsm {
+    new(options: FsmOptions): BehavioralFsm;
+    static extend(options: FsmOptions): typeof BehavioralFsm;
+
+    initialState: string;
+    eventListeners: EventListeners;
+    states: States;
+    namespace?: string;
+    initialize: () => void;
+
+    emit(eventName: string, ...args: any[]): void;
+    handle(client: Client, eventName: string, ...args: any[]): void;
+    transition(client: Client, stateName: string): void;
+    processQueue(client: Client, type: string): void;
+    clearQueue(client: Client, type?: string, stateName?: string): void;
+    deferUntilTransition(client: Client, stateName?: string): void;
+    deferAndTransition(client: Client, stateName: string): void;
+    compositeState(client: Client): string;
+    on(eventName: string, callback: EventListener): EventOnResult;
+    off(eventName?: string, callback?: EventListener): void;
+  }
+
+  export class Fsm implements ClientInstance {
+    new(options: FsmOptions): Fsm;
+    static extend(options: FsmOptions): typeof Fsm;
+
+    initialState: string;
+    eventListeners: EventListeners;
+    states: States;
+    namespace?: string;
+    initialize: () => void;
+
+    targetReplayState: any;
+    state: string;
+    priorState: string;
+    priorAction: string;
+    currentAction: string;
+    currentActionArgs: any[];
+    inputQueue: any[];
+    inExitHandler: boolean;
 
     emit(eventName: string, ...args: any[]): void;
     handle(eventName: string, ...args: any[]): void;
