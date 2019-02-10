1afea610e46fc2b18243ce70369b1e20e573bde9
Alex Hamlin <alex@alexhamlin.co>
2019-02-10 14:01:02 -0800

    Update comment about machina typings
    These are much more comprehensive than when I started. I'd still like to
    take another pass through the API, then I'd like to consider breaking
    these out and contributing to machina.js (assuming others haven't
    already done so by the time I get there).

--- server/machina.d.ts
+++ server/machina.d.ts
@@ -1,8 +1,5 @@
-// TODO: Declare stricter types. Consider contributing to machina.js itself [0]
-// or DefinitelyTyped [1] (but preferably the former).
-//
-// [0]: https://github.com/ifandelse/machina.js/issues/158
-// [1]: https://github.com/DefinitelyTyped/DefinitelyTyped/issues/25075
+// TODO: Continue improving and validating these declarations, then contribute
+// to machina.js (https://github.com/ifandelse/machina.js/issues/158).
 declare module 'machina' {
   // export as namespace machina;
 
