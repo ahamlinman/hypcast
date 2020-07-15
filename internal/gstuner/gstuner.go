1a89469f2419328d089513b5944fb5f252bc72ad
Alex Hamlin <alex@alexhamlin.co>
2020-07-15 00:08:21 -0700

    Credit https://github.com/pion/rtwatch where it's due
    Without that demo I never would have gotten nearly this far.

--- internal/gstuner/gstuner.go
+++ internal/gstuner/gstuner.go
@@ -1,5 +1,8 @@
 // Package gstuner supports the management of GStreamer pipelines that process
 // live TV signals from DVB devices.
+//
+// The basic concepts behind this integration are heavily inspired by
+// https://github.com/pion/rtwatch.
 package gstuner
 
 // #cgo pkg-config: gstreamer-1.0
