02a8e34358abce3bb4d747c2da94b4fe4496e0c4
Alex Hamlin <alex@alexhamlin.co>
2020-07-19 22:47:13 -0700

    Add initial atsc package for channel parsing
    This is the first step toward being able to watch more than the PBS
    station I've hardcoded for Seattle.

---
+++ internal/atsc/atsc.go
@@ -0,0 +1,118 @@
+// Package atsc provides representations of ATSC television channel information
+// for use by a TV tuner card.
+//
+// For the purposes of this package, channels.conf files are those using the
+// azap-compatible format described at
+// https://www.mythtv.org/wiki/Adding_Digital_Cable_Channels_(For_ATSC/QAM_Tuner_Cards_--_USA/Canada)#channels.conf_Format.
+// The https://github.com/stefantalpalaru/w_scan2 utility is useful for
+// generating a compatible file:
+//
+//   ./w_scan2 -f a -c us -X >> channels.conf
+package atsc
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"strconv"
+	"strings"
+)
+
+// Modulation represents the modulation of an ATSC television channel.
+type Modulation string
+
+// Normalized Modulation values for a Channel.
+const (
+	Modulation8VSB   Modulation = "8VSB"
+	ModulationQAM64             = "QAM_64"
+	ModulationQAM256            = "QAM_256"
+)
+
+// numChannelFields is used to ensure that each channels.conf line is valid. It
+// must match the number of fields in Channel.
+const numChannelFields = 6
+
+// Channel represents the definition of an ATSC television channel.
+type Channel struct {
+	Name       string
+	Frequency  uint
+	Modulation Modulation
+	VideoPID   uint
+	AudioPID   uint
+	ProgramID  uint
+}
+
+func (c Channel) String() string {
+	return fmt.Sprintf(
+		"%s:%d:%s:%d:%d:%d",
+		c.Name, c.Frequency, c.Modulation, c.VideoPID, c.AudioPID, c.ProgramID,
+	)
+}
+
+// ParseChannelsConf parses Channels from a channels.conf file.
+func ParseChannelsConf(r io.Reader) ([]Channel, error) {
+	var (
+		channels []Channel
+		err      error
+		line     = 0
+		scanner  = bufio.NewScanner(r)
+	)
+
+	for scanner.Scan() {
+		line++
+
+		fields := strings.Split(scanner.Text(), ":")
+		if len(fields) != numChannelFields {
+			return nil, fmt.Errorf(
+				"channels.conf line %d has %d fields, expected %d",
+				line, len(fields), numChannelFields,
+			)
+		}
+
+		parseUint := func(s string) uint {
+			i, ierr := strconv.ParseUint(s, 10, 0)
+			if ierr != nil && err == nil {
+				err = fmt.Errorf("channels.conf line %d has invalid field value %q", line, s)
+			}
+			return uint(i)
+		}
+
+		parseModulation := func(s string) Modulation {
+			switch s {
+			case "8VSB":
+				fallthrough
+			case "VSB_8":
+				return Modulation8VSB
+
+			case "QAM_64":
+				return ModulationQAM64
+
+			case "QAM_256":
+				return ModulationQAM256
+			}
+
+			if err == nil {
+				err = fmt.Errorf("channels.conf line %d has unknown modulation %q", line, s)
+			}
+			return Modulation("") // Invalid; should never leave this function
+		}
+
+		channels = append(channels, Channel{
+			Name:       fields[0],
+			Frequency:  parseUint(fields[1]),
+			Modulation: parseModulation(fields[2]),
+			VideoPID:   parseUint(fields[3]),
+			AudioPID:   parseUint(fields[4]),
+			ProgramID:  parseUint(fields[5]),
+		})
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, fmt.Errorf("unable to read channels.conf: %w", err)
+	}
+
+	return channels, nil
+}
---
+++ internal/atsc/atsc_test.go
@@ -0,0 +1,90 @@
+package atsc
+
+import (
+	"reflect"
+	"strings"
+	"testing"
+)
+
+const validChannelsConf = `KCTS-HD:189000000:8VSB:49:52:3
+KIDS:189000000:8VSB:65:68:4
+CREATE:189000000:8VSB:81:84:5
+WORLD:189000000:8VSB:97:100:6`
+
+func TestParseChannelsConf(t *testing.T) {
+	testCases := []struct {
+		name    string
+		input   string
+		want    []Channel
+		wantErr bool
+	}{
+		{
+			name:  "valid channels.conf",
+			input: validChannelsConf,
+			want: []Channel{
+				{"KCTS-HD", 189_000_000, Modulation8VSB, 49, 52, 3},
+				{"KIDS", 189_000_000, Modulation8VSB, 65, 68, 4},
+				{"CREATE", 189_000_000, Modulation8VSB, 81, 84, 5},
+				{"WORLD", 189_000_000, Modulation8VSB, 97, 100, 6},
+			},
+		},
+
+		{
+			name:  "w_scan2 nonstandard 8VSB output",
+			input: "KCTS-HD:189000000:VSB_8:49:52:3",
+			want: []Channel{
+				{"KCTS-HD", 189_000_000, Modulation8VSB, 49, 52, 3},
+			},
+		},
+
+		{
+			name:  "QAM256 modulation",
+			input: "WLFI:255000000:QAM_256:66:68:4",
+			want: []Channel{
+				{"WLFI", 255_000_000, ModulationQAM256, 66, 68, 4},
+			},
+		},
+
+		{
+			name:    "wrong number of fields",
+			input:   "KCTS-HD:189000000:8VSB:3",
+			wantErr: true,
+		},
+
+		{
+			name:    "invalid frequency",
+			input:   "KCTS-HD:189.0123456:8VSB:49:52:3",
+			wantErr: true,
+		},
+
+		{
+			name:    "invalid modulation",
+			input:   "KCTS-HD:189000000:42VSB:49:52:3",
+			wantErr: true,
+		},
+
+		{
+			name:    "invalid PID",
+			input:   "KCTS-HD:189000000:8VSB:49:52:?",
+			wantErr: true,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			got, err := ParseChannelsConf(strings.NewReader(tc.input))
+			if err != nil {
+				if !tc.wantErr {
+					t.Fatalf("unexpected error: %v", err)
+				}
+
+				t.Logf("error: %v", err)
+				return
+			}
+
+			if !reflect.DeepEqual(got, tc.want) {
+				t.Errorf("unexpected result\ngot:  %v\nwant: %v", got, tc.want)
+			}
+		})
+	}
+}
