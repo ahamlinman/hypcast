61e1f4171d88a9931c07cc8c7d4d0f483ea92343
Alex Hamlin <alex@alexhamlin.co>
2020-10-12 21:16:44 -0700

    Start integrating new API into server
    The new API is currently a stub providing the channel list, so it's not
    very useful yet. However the old API is still available to maintain
    parity.

--- cmd/hypcast-server/main.go
+++ cmd/hypcast-server/main.go
@@ -11,42 +11,64 @@ import (
 	"syscall"
 	"time"
 
+	"github.com/ahamlinman/hypcast/internal/api"
 	"github.com/ahamlinman/hypcast/internal/atsc"
 	"github.com/ahamlinman/hypcast/internal/atsc/tuner"
 	"github.com/ahamlinman/hypcast/internal/oldclient"
 )
 
 func main() {
-	var channelsConf string
-	flag.StringVar(&channelsConf, "channels", "/etc/hypcast/channels.conf", "Path to channels.conf")
+	var (
+		flagAddr = flag.String(
+			"addr", ":9200",
+			"Address for the HTTP server to listen on",
+		)
+		flagChannels = flag.String(
+			"channels", "/etc/hypcast/channels.conf",
+			"Path to the channels.conf file containing the list of available channels",
+		)
+	)
 	flag.Parse()
 
-	log.Print("Reading channels.conf")
-	f, err := os.Open(channelsConf)
-	if err != nil {
-		log.Fatalf("Unable to open channels.conf: %v", err)
-	}
-	channels, err := atsc.ParseChannelsConf(f)
-	f.Close()
+	channels, err := readChannelsConf(*flagChannels)
 	if err != nil {
 		log.Fatalf("Unable to read channels.conf: %v", err)
 	}
 
 	tuner := tuner.NewTuner(channels)
 
-	http.Handle("/config/channels", oldclient.ChannelListHandler(tuner.Channels()))
-	http.Handle("/old-control-socket", oldclient.TunerControlHandler(tuner))
+	mux := http.NewServeMux()
 
-	log.Print("Starting web server")
-	server := http.Server{Addr: ":9200"}
+	// TODO: Legacy APIs, remove once new API is fully implemented.
+	mux.Handle("/config/channels", oldclient.ChannelListHandler(tuner.Channels()))
+	mux.Handle("/old-control-socket", oldclient.TunerControlHandler(tuner))
+
+	// New API
+	mux.Handle("/api", api.NewHandler(tuner))
+
+	log.Printf("Starting Hypcast server on %s", *flagAddr)
+	server := http.Server{
+		Addr:    *flagAddr,
+		Handler: mux,
+	}
 	go server.ListenAndServe()
 
 	signalCh := make(chan os.Signal)
 	signal.Notify(signalCh, os.Interrupt, syscall.SIGTERM)
 	<-signalCh
 
-	log.Print("Stopping web server")
+	log.Print("Shutting down")
 	stopCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 	server.Shutdown(stopCtx)
+}
+
+func readChannelsConf(path string) ([]atsc.Channel, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	return atsc.ParseChannelsConf(f)
 }
---
+++ internal/api/api.go
@@ -0,0 +1,65 @@
+package api
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/ahamlinman/hypcast/internal/atsc/tuner"
+)
+
+// Handler serves the Hypcast API for a single tuner.
+type Handler struct {
+	mux   *http.ServeMux
+	tuner *tuner.Tuner
+}
+
+// NewHandler creates a Handler serving the Hypcast API for tuner.
+func NewHandler(tuner *tuner.Tuner) *Handler {
+	h := &Handler{
+		mux:   http.NewServeMux(),
+		tuner: tuner,
+	}
+
+	h.mux.HandleFunc("/api/config/channels", h.handleConfigChannels)
+
+	h.mux.HandleFunc("/api/rpc/stop", h.handleRPCStop)
+	h.mux.HandleFunc("/api/rpc/tune", h.handleRPCTune)
+
+	h.mux.HandleFunc("/api/sockets/rtc-signaling", h.handleSocketRTCSignaling)
+	h.mux.HandleFunc("/api/sockets/tuner-status", h.handleSocketTunerStatus)
+
+	return h
+}
+
+func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	h.mux.ServeHTTP(w, r)
+}
+
+func (h *Handler) handleConfigChannels(w http.ResponseWriter, r *http.Request) {
+	var (
+		channels = h.tuner.Channels()
+		names    = make([]string, len(channels))
+	)
+	for i, ch := range channels {
+		names[i] = ch.Name
+	}
+
+	w.Header().Add("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(names)
+}
+
+func (h *Handler) handleRPCStop(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNoContent)
+}
+
+func (h *Handler) handleRPCTune(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNoContent)
+}
+
+func (h *Handler) handleSocketRTCSignaling(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNoContent)
+}
+
+func (h *Handler) handleSocketTunerStatus(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNoContent)
+}
