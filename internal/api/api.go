c3f5d69eca6eacf3311179ce608056999953c241
Alex Hamlin <alex@alexhamlin.co>
2020-10-12 23:25:32 -0700

    Move RPC API methods to their own file

--- internal/api/api.go
+++ internal/api/api.go
@@ -2,7 +2,6 @@ package api
 
 import (
 	"encoding/json"
-	"errors"
 	"net/http"
 
 	"github.com/ahamlinman/hypcast/internal/atsc/tuner"
@@ -26,7 +25,7 @@ func NewHandler(tuner *tuner.Tuner) *Handler {
 	h.mux.HandleFunc("/api/rpc/stop", h.handleRPCStop)
 	h.mux.HandleFunc("/api/rpc/tune", h.handleRPCTune)
 
-	h.mux.HandleFunc("/api/sockets/rtc-signaling", h.handleSocketRTCSignaling)
+	h.mux.HandleFunc("/api/sockets/webrtc-signaling", h.handleSocketWebRTCSignaling)
 	h.mux.HandleFunc("/api/sockets/tuner-status", h.handleSocketTunerStatus)
 
 	return h
@@ -49,55 +48,7 @@ func (h *Handler) handleConfigChannels(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(names)
 }
 
-func (h *Handler) handleRPCStop(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		return
-	}
-
-	if err := h.tuner.Stop(); err != nil {
-		w.Header().Add("Content-Type", "application/json")
-		w.WriteHeader(http.StatusInternalServerError)
-		json.NewEncoder(w).Encode(struct{ Error string }{err.Error()})
-		return
-	}
-
-	w.WriteHeader(http.StatusNoContent)
-}
-
-func (h *Handler) handleRPCTune(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		return
-	}
-
-	var params struct{ ChannelName string }
-	err := json.NewDecoder(r.Body).Decode(&params)
-	if err != nil || params.ChannelName == "" {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	err = h.tuner.Tune(params.ChannelName)
-
-	if err != nil {
-		w.Header().Add("Content-Type", "application/json")
-
-		switch {
-		case errors.Is(err, tuner.ErrChannelNotFound):
-			w.WriteHeader(http.StatusBadRequest)
-		default:
-			w.WriteHeader(http.StatusInternalServerError)
-		}
-
-		json.NewEncoder(w).Encode(struct{ Error string }{err.Error()})
-		return
-	}
-
-	w.WriteHeader(http.StatusNoContent)
-}
-
-func (h *Handler) handleSocketRTCSignaling(w http.ResponseWriter, r *http.Request) {
+func (h *Handler) handleSocketWebRTCSignaling(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusNoContent)
 }
 
