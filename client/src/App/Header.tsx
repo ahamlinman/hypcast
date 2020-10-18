1c04839a04e6413c8805abc5192d398f9ac67a87
Alex Hamlin <alex@alexhamlin.co>
2020-10-18 14:49:24 -0700

    Additional header tweaks
    Still working this out. Think I might want to use grid instead of flex
    to get things laid out the way I want across desktop and mobile.

--- client/src/App/Header.tsx
+++ client/src/App/Header.tsx
@@ -1,38 +1,27 @@
 import React from "react";
 
-import { useWebRTC } from "../WebRTC";
+import { useWebRTC, State as WebRTCState } from "../WebRTC";
 import { useTunerStatus, Status as TunerStatus } from "../TunerStatus";
 import rpc from "../rpc";
 
 export default function Header() {
-  const webRTC = useWebRTC();
-  const tunerStatus = useTunerStatus();
-
   return (
     <header className="Header">
-      <PowerButton
-        active={
-          tunerStatus.Connection === "Connected" &&
-          tunerStatus.State === "Playing"
-        }
-      />
-      <h1>hypcast</h1>
-      <StatusIndicator
-        description={`WebRTC ${webRTC.Connection.Status}`}
-        active={webRTC.Connection.Status === "Connected"}
-      />
-      <StatusIndicator
-        description={`Tuner ${tunerStatusToString(tunerStatus)}`}
-        active={
-          tunerStatus.Connection === "Connected" &&
-          tunerStatus.State === "Playing"
-        }
-      />
+      <PowerButton />
+      <Title />
+      <StatusIndicator />
     </header>
   );
 }
 
-function PowerButton({ active }: { active: boolean }) {
+function Title() {
+  return <h1>hypcast</h1>;
+}
+
+function PowerButton() {
+  const tunerStatus = useTunerStatus();
+  const active = isTunerActive(tunerStatus);
+
   return (
     <button
       className={`PowerButton ${active ? "PowerButton--Active" : ""}`}
@@ -43,13 +32,12 @@ function PowerButton({ active }: { active: boolean }) {
   );
 }
 
-function StatusIndicator({
-  active,
-  description,
-}: {
-  active: boolean;
-  description: string;
-}) {
+function StatusIndicator() {
+  const webRTC = useWebRTC();
+  const tunerStatus = useTunerStatus();
+
+  const active = isActive(webRTC, tunerStatus);
+
   return (
     <div className="StatusIndicator">
       <div
@@ -57,22 +45,50 @@ function StatusIndicator({
           active ? "StatusIndicator__Dot--Active" : ""
         }`}
       ></div>
-      <span className="StatusIndicator__Description">{description}</span>
+      <span className="StatusIndicator__Description">
+        {statusString(webRTC, tunerStatus)}
+      </span>
     </div>
   );
 }
 
-const tunerStatusToString = (status: TunerStatus) => {
+function statusString(webRTC: WebRTCState, tunerStatus: TunerStatus): string {
+  const webRTCString = webRTCStatusString(webRTC);
+  if (webRTCString !== undefined) {
+    return webRTCString;
+  }
+
+  return tunerStatusString(tunerStatus);
+}
+
+function webRTCStatusString(state: WebRTCState): string | undefined {
+  if (state.Connection.Status !== "Connected") {
+    return state.Connection.Status;
+  }
+
+  return undefined;
+}
+
+function tunerStatusString(status: TunerStatus) {
   if (status.Connection !== "Connected") {
-    return `${status.Connection}`;
+    return status.Connection;
   }
 
   if (status.State === "Stopped") {
-    if (status.Error !== undefined) {
-      return `${status.State} (${status.Error})`;
-    }
     return status.State;
   }
 
   return `${status.State} ${status.ChannelName}`;
-};
+}
+
+function isActive(webRTC: WebRTCState, tunerStatus: TunerStatus): boolean {
+  if (webRTC.Connection.Status !== "Connected") {
+    return false;
+  }
+
+  return isTunerActive(tunerStatus);
+}
+
+function isTunerActive(status: TunerStatus) {
+  return status.Connection === "Connected" && status.State === "Playing";
+}
