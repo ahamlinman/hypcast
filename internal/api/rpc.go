09e72fdd33cac4e4d20b40690e8043f3cf2e54c4
Alex Hamlin <alex@alexhamlin.co>
2021-09-18 01:09:23 -0700

    Clean up the RPC handler a bit
    Been meaning to do something like this for a while.

--- internal/api/rpc.go
+++ internal/api/rpc.go
@@ -47,33 +47,24 @@ func handleRPC(handler rpcHandlerFunc) http.Handler {
 			return
 		}
 
-		params, err := readRPCParams(r)
-
 		var (
-			code int
-			body interface{}
+			code         int
+			body         interface{}
+			params, perr = readRPCParams(r)
 		)
-		switch {
-		case errors.Is(err, errBodyTooLarge):
-			code, body = http.StatusRequestEntityTooLarge, err
-		case errors.Is(err, errInvalidBodyType):
-			code, body = http.StatusUnsupportedMediaType, err
-		case errors.Is(err, errInvalidBody):
-			code, body = http.StatusBadRequest, err
-		case err != nil:
-			code, body = http.StatusInternalServerError, err
-
-		default:
+		if perr == nil {
 			code, body = handler(params)
+		} else {
+			code, body = errorHTTPCode(perr), perr
 		}
 
-		if err, ok := body.(error); ok {
-			body = struct{ Error string }{err.Error()}
+		if berr, ok := body.(error); ok {
+			body = struct{ Error string }{berr.Error()}
 		}
-
 		if body != nil {
 			w.Header().Add("Content-Type", "application/json")
 		}
+
 		w.WriteHeader(code)
 		if body != nil {
 			json.NewEncoder(w).Encode(body)
@@ -81,16 +72,23 @@ func handleRPC(handler rpcHandlerFunc) http.Handler {
 	})
 }
 
-const maxBodySize = 1024
+type httpError struct {
+	HTTPCode int
+	Message  string
+}
+
+func (h httpError) Error() string { return h.Message }
 
 var (
-	errBodyTooLarge    = errors.New("RPC body exceeded maximum size")
-	errInvalidBodyType = errors.New("must have Content-Type: application/json")
-	errInvalidBody     = errors.New("failed to parse RPC body as JSON")
+	errBodyTooLarge    = httpError{http.StatusRequestEntityTooLarge, "RPC body exceeded maximum size"}
+	errInvalidBodyType = httpError{http.StatusUnsupportedMediaType, "must have Content-Type: application/json"}
+	errInvalidBody     = httpError{http.StatusBadRequest, "failed to parse RPC body as JSON"}
 )
 
 func readRPCParams(r *http.Request) (rpcParams, error) {
+	const maxBodySize = 1024
 	var body bytes.Buffer
+
 	n, err := body.ReadFrom(io.LimitReader(r.Body, maxBodySize+1))
 	switch {
 	case n == 0 || err != nil:
@@ -103,11 +101,19 @@ func readRPCParams(r *http.Request) (rpcParams, error) {
 		return nil, errInvalidBodyType
 	}
 
-	var params map[string]interface{}
+	var params rpcParams
 	err = json.Unmarshal(body.Bytes(), &params)
 	if err != nil {
 		return nil, errInvalidBody
 	}
 
 	return params, nil
+}
+
+func errorHTTPCode(err error) int {
+	var herr httpError
+	if errors.As(err, &herr) {
+		return herr.HTTPCode
+	}
+	return http.StatusInternalServerError
 }
