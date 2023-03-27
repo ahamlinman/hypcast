7aa49a7de0ce3aa519c1c9f221865a00de0f1c44
Alex Hamlin <alex@alexhamlin.co>
2023-03-26 19:50:45 -0700

    Update Go doc comments around the client build

--- client/client.go
+++ client/client.go
@@ -3,7 +3,8 @@ package client
 
 import "io/fs"
 
-// Build embeds the Hypcast client when the "embedclient" build tag is set. When
-// Build is nil, embedded client assets are not available. When it is not nil,
-// it is rooted inside of the "build" directory produced by the build process.
+// Build embeds the Hypcast client when the "embedclientzip" build tag is set.
+// When Build is nil, embedded client assets are not available. When it is not
+// nil, it is rooted inside of the output directory produced by the client build
+// process, such that index.html will be at the root.
 var Build fs.FS
